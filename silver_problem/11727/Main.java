c259311a0e495525ee2f24eac38106e0cacee2c9
JunSeok-Yun <ujs9901@naver.com>
2025-04-03 14:51:46 +0900

    Solve: BOJ Silver 11727

---
+++ silver_problem/11727/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[N]);
+    }
+}
